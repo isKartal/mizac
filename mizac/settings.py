129859d28145d3aea88183a512056d3706db1688
Omgorege <omgorege@example.com>
2025-06-02 14:01:43 +0300

    google hosting

--- mizac/settings.py
+++ mizac/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--7$tmakopcdtqrpjvl##@f-$qu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['4mizac1234.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
